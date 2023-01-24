18cfa3c5c938bfc9d96c6f2c34a181c27494e21e
unknown <mihaela@matrixinformatika.hr>
2023-01-24 13:50:36 +0100

    More complex reducer logic

--- src/components/Cart/Cart.js
+++ src/components/Cart/Cart.js
@@ -1,14 +1,39 @@
+import { useContext } from "react";
 import classes from "./Cart.module.css";
 import Modal from "../UI/Modal";
+import CartContext from "../../store/cart-context";
+import CartItem from "./CartItem";
 
 const Cart = (props) => {
+  const cartCtx = useContext(CartContext);
+  const totalAmount = `$${cartCtx.totalAmount.toFixed(2)}`;
+  const hasItems = cartCtx.items.length > 0;
+
+  const cartItemRemoveHandler = (id) => {};
+  const cartItemAddHandler = (item) => {};
+
+  // If we want to pass additional params (not only the event which is passed by default), we can't just write a simple reference:
+  // onClick={clickHandler}
+  // And we can't write ...
+  // onClick={clickHandler(param)}
+  // ... since this would call the function immediately (and not only when the cart item is clicked).
+  // So, if we want to pass params, we can either use bind (the first param is not used here, so we can write anything in this place)
+
+  // bind pre-configure a function for future execution and basically allows you to
+  // pre-configure the argument that function will receive when it's being executed.
+
   const cartItems = (
     <ul className={classes["cart-items"]}>
-      {[{ key: 1, id: "c1", name: "Sushi", amount: 2, price: 12.99 }].map(
-        (item) => (
-          <li key={item.key}>{item.name}</li>
-        )
-      )}
+      {cartCtx.items.map((item) => (
+        <CartItem
+          key={item.id}
+          name={item.name}
+          price={item.price}
+          amount={item.amount}
+          onRemove={cartItemRemoveHandler.bind(null, item.id)}
+          onAdd={cartItemAddHandler.bind(null, item)}
+        />
+      ))}
     </ul>
   );
   return (
@@ -16,13 +41,13 @@ const Cart = (props) => {
       {cartItems}
       <div className={classes.total}>
         <span>Total Amount</span>
-        <span>34.54</span>
+        <span>{totalAmount}</span>
       </div>
       <div className={classes.actions}>
         <button className={classes["button--alt"]} onClick={props.onCloseCart}>
           Close
         </button>
-        <button className={classes.button}>Order</button>
+        {hasItems && <button className={classes.button}>Order</button>}
       </div>
     </Modal>
   );
